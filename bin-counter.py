0de577ec53608c2df0b86a46a537c2455e8a3afc
Ákos Nikházy <nikhazy.akos@gmail.com>
2022-08-15 18:35:53 +0200

    Create bin-counter.py

---
+++ bin-counter.py
@@ -0,0 +1,42 @@
+from machine import Pin
+from time import sleep
+
+# our bits
+bit_pins = [
+    Pin(0,Pin.OUT),
+    Pin(1,Pin.OUT),
+    Pin(2,Pin.OUT),
+    Pin(3,Pin.OUT),
+    Pin(4,Pin.OUT),
+    Pin(5,Pin.OUT),
+    Pin(6,Pin.OUT),
+    Pin(7,Pin.OUT)
+]
+
+
+counter = 0
+
+while True:
+    
+    # list of bits from converted int to bin to string
+    num_now = list(str(bin(counter)[2:]))
+    
+    # zfill is not present in micropython :)
+    padding_list = []
+    for i in range(0,8-len(num_now)):
+       padding_list.append('0');
+       
+    # so I padding the list instead with another list mad up from zeroes
+    num_now = padding_list + num_now
+    
+    # so I can use this loop to switch leds on PINs
+    for i in range(len(num_now)):
+        bit_pins[i].value(int(num_now[i]))
+    
+    # we grow the counter by one and restart if we are at limit. 8 bits is 255 max
+    counter += 1
+    if counter == 256:
+        counter = 0
+    
+    # so we see what happens.
+    sleep(0.25)
